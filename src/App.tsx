34df20503e4b3deeb0523b938fbbdc46428c7454
Welison Wessling <wwelisonw@gmail.com>
2023-09-12 14:56:04 -0300

    feat: created textareas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Button } from "./components/ui/button";
 import { Github } from 'lucide-react'
 import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
 
@@ -23,8 +24,28 @@ export function App() {
         </div>
       </header>
 
-     
+      <main className="flex-1 p-6 flex gap-6">
+        <section className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea 
+              placeholder="Inclua o prompt para a IA..."
+              className="resize-none p-5 leading-relaxed"
+            />
 
+            <Textarea 
+              placeholder="Resultado gerado pela IA..."
+              className="resize-none p-5 leading-relaxed"
+              readOnly 
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável <code className="text-red-500">&#123;transcription&#125;</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+          </p>
+        </section>
+
+        
+      </main>
     </div>
   );
 };
