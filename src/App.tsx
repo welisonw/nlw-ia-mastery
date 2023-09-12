b4d17bc5b2d5c965e90779d805009024b7193a47
Welison Wessling <wwelisonw@gmail.com>
2023-09-12 14:35:50 -0300

    feat: created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { Button } from "./components/ui/button";
 
+import { Button } from "./components/ui/button";
+import { Github } from 'lucide-react'
+import { Separator } from "./components/ui/separator";
 
 export function App() {
 
   return (
+    <div className="min-h-screen flex flex-col">
+      <header className="px-6 py-3 flex items-center justify-between border-b ">
+        <h1 className="text-xl font-bold">upload.ai</h1>
 
-    <Button>teste</Button>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="default">
+            < Github className="w-4 h-4 mr-2"/>Github
+          </Button>
+        </div>
+      </header>
+
+     
+
+    </div>
   );
 };
